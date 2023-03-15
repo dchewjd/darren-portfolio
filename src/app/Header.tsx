01b1c2b7e0cd885ae1adb13efa218cec2f7ff55e
unknown <darrencjd@gmail.com>
2023-03-15 12:27:50 +0800

    deploy

---
+++ src/app/Header.tsx
@@ -0,0 +1,141 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { RiMenu3Fill, RiCloseFill } from "react-icons/ri";
+
+export const handleClickScroll = (id: string) => {
+  const element = document.getElementById(id);
+  if (element) {
+    // 👇 Will scroll smoothly to the top of the next section
+    element.scrollIntoView({ behavior: "smooth" });
+  }
+};
+
+const Header = () => {
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [visible, setVisible] = useState(true);
+  const [hamburgerMenuOpen, setHamburgerMenuOpen] = useState(false);
+
+  const handleScroll = () => {
+    const currentScrollPos = window.scrollY;
+
+    if (currentScrollPos > prevScrollPos) {
+      setVisible(false);
+    } else {
+      setVisible(true);
+    }
+
+    setPrevScrollPos(currentScrollPos);
+  };
+
+  const handleHamburgerClickScroll = (id: string) => {
+    setHamburgerMenuOpen((prev) => !prev);
+    handleClickScroll(id);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  });
+
+  useEffect(() => {
+    console.log("hamburgerMenuOpen", hamburgerMenuOpen);
+  }, [hamburgerMenuOpen]);
+
+  return (
+    <nav
+      className={`${
+        visible ? "top-0" : ""
+      } bg-gray-800 sticky flex flex-row justify-between`}
+    >
+      <a
+        href="/"
+        className="mx-8 my-4 w-fit text-orange-400 font-serif text-5xl flex justify-start"
+      >
+        D
+      </a>
+      {/* desktop nav */}
+      <div className="font-sfmono text-sm hidden md:visible md:flex justify-end">
+        <button
+          onClick={() => handleClickScroll("about")}
+          className="px-8 py-6 min-w-fit text-slate-300 hover:text-orange-400"
+        >
+          <span className="text-orange-400">01.</span> About
+        </button>
+        <button
+          onClick={() => handleClickScroll("experience")}
+          className="px-8 py-6 min-w-fit text-slate-300 hover:text-orange-400"
+        >
+          <span className="text-orange-400">02.</span> Experience
+        </button>
+        <button
+          onClick={() => handleClickScroll("work")}
+          className="px-8 py-6 min-w-fit text-slate-300 hover:text-orange-400"
+        >
+          <span className="text-orange-400">03.</span> Work
+        </button>
+        <button
+          onClick={() => handleClickScroll("contact")}
+          className="px-8 py-6 min-w-fit text-slate-300 hover:text-orange-400"
+        >
+          <span className="text-orange-400">04.</span> Contact
+        </button>
+      </div>
+      {/* mobile nav */}
+      <div className="my-5 mr-12 md:hidden flex justify-center z-10 cursor-pointer ">
+        <RiMenu3Fill
+          onClick={() => setHamburgerMenuOpen((prev) => !prev)}
+          size="2.5rem"
+          className={`text-slate-400 ${
+            hamburgerMenuOpen ? "hidden" : "absolute"
+          }`}
+        />
+        <RiCloseFill
+          size="2.5rem"
+          onClick={() => setHamburgerMenuOpen((prev) => !prev)}
+          className={`text-slate-400 ${hamburgerMenuOpen ? "fixed" : "hidden"}`}
+        />
+      </div>
+      <div
+        className={`bg-gray-800 w-screen h-screen flex flex-col gap-16 justify-center ${
+          hamburgerMenuOpen ? "fixed" : "hidden"
+        }`}
+      >
+        <a
+          onClick={() => {
+            handleHamburgerClickScroll("about");
+          }}
+          className="place-self-center cursor-pointer text-2xl"
+        >
+          01. About
+        </a>
+        <a
+          onClick={() => {
+            handleHamburgerClickScroll("experience");
+          }}
+          className="place-self-center cursor-pointer text-2xl"
+        >
+          02. Experience
+        </a>
+        <a
+          onClick={() => {
+            handleHamburgerClickScroll("work");
+          }}
+          className="place-self-center cursor-pointer text-2xl"
+        >
+          03. Work
+        </a>
+        <a
+          onClick={() => {
+            handleHamburgerClickScroll("contact");
+          }}
+          className="place-self-center cursor-pointer text-2xl"
+        >
+          04. Contact
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,91 +1,266 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
+"use client";
 
-const inter = Inter({ subsets: ['latin'] })
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import React from "react";
+import * as Tabs from "@radix-ui/react-tabs";
+import { RiLinkedinBoxFill, RiMailFill, RiGithubFill } from "react-icons/ri";
+import { handleClickScroll } from "./Header";
+import motivateAiScreen from "../../public/motivate-ai-screencap.png";
+import darrenPortfolioScreen from "../../public/darren-portfolio-screencap.png";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
+    <div
+      className={`bg-gray-800 px-8 sm:px-16 md:px-24 flex flex-col items-center justify-center`}
+    >
+      <div
+        id="hero"
+        className="max-w-2xl h-screen flex flex-col justify-center gap-1 md:gap-2"
+      >
+        <p className="text-orange-400 tracking-normal leading-4 md:leading-10 font-sfmono">
+          Hi, my name is
         </p>
-        <div>
+        <br />
+        <h1 className="text-slate-300 ml-[-4px]">Darren Chew.</h1>
+        <h1 className="text-slate-400 ml-[-3px]">
+          I craft delightful web experiences.
+        </h1>
+        <br />
+        <p className="text-slate-400 max-w-2xl pb-4">
+          I’m a software engineer specializing in building (and occasionally
+          designing) exceptional digital experiences.
+        </p>
+        <br />
+        <button
+          className="text-md px-2 pt-3 pb-2 w-1/3 text-orange-400 border rounded-md border-orange-400 justify-items-start hover:bg-orange-400/80 hover:text-gray-800 "
+          onClick={() => handleClickScroll("about")}
+        >
+          About Me
+        </button>
+      </div>
+      <div id="about" className="max-w-2xl py-24 ">
+        <p className="font-sfmono text-orange-400">
+          01.{" "}
+          <span className="text-3xl font-semibold text-slate-300">
+            About Me
+          </span>
+        </p>
+        <br />
+        <p>
+          Hello! My name is Darren, and I'm a software engineer with 3+ years of
+          experience in creating engaging and dynamic web interfaces. My passion
+          is crafting digital experiences that are both beautiful and
+          functional, and that have a real impact on people's lives.
+        </p>
+        <br />
+        <p>
+          I'm constantly striving to stay up-to-date with the latest trends and
+          technologies to deliver cutting-edge solutions that meet the demands
+          of today's fast-paced digital landscape.
+        </p>
+        <br />
+        <p>Here are a few technologies I’ve been working with recently:</p>
+        <ul className="md:w-2/3 list-disc list-inside grid grid-rows-3 grid-flow-col marker:text-orange-400 gap-1">
+          <li>JavaScript (ES6+)</li>
+          <li>TypeScript</li>
+          <li>React</li>
+          <li>Next.js</li>
+          <li>Tailwind CSS</li>
+        </ul>
+        <br />
+        <p>
+          At my core, I'm a builder and a creator. I'm passionate about using
+          technology to drive success and create meaningful solutions that make
+          a difference in people's lives. If you're looking for a skilled and
+          dedicated software engineer to bring your digital vision to life, I'd
+          love to hear from you.
+        </p>
+      </div>
+      <div id="experience" className="max-w-2xl py-24">
+        <p className="font-sfmono text-orange-400">
+          02.{" "}
+          <span className="text-3xl font-semibold text-slate-300">
+            Where I've worked
+          </span>
+        </p>
+        <Tabs.Root
+          defaultValue="tab1"
+          // orientation="vertical"
+          className="m-1"
+        >
+          <Tabs.List
+            aria-label="tabs example"
+            className="font-sfmono text-sm  text-slate-400 my-4"
+          >
+            <Tabs.Trigger
+              value="tab1"
+              className="text-center h-12 w-36 mb-4 px-6 pt-1 border-b-2 data-[state=inactive]:border-slate-400/30 data-[state=active]:border-orange-400 data-[state=active]:text-orange-400 hover:bg-slate-600"
+            >
+              Alpha Impact
+            </Tabs.Trigger>
+            <Tabs.Trigger
+              value="tab2"
+              className="text-center h-12 w-36 mb-4 px-6 pt-1 border-b-2 data-[state=inactive]:border-slate-400/30 data-[state=active]:border-orange-400 data-[state=active]:text-orange-400 hover:bg-slate-600"
+            >
+              NCS
+            </Tabs.Trigger>
+          </Tabs.List>
+          <Tabs.Content value="tab1" className="">
+            <p className="text-slate-300">
+              Software Engineer
+              <span className="text-orange-400"> @ Alpha Impact</span>
+            </p>
+            <p className="text-sm">November 2021 - February 2022</p>
+            <br />
+            <ul className="ml-4 list-disc list-outside marker:text-orange-400 gap-1 space-y-4 ">
+              <li>
+                Designed and developed a highly engaging user interface for a
+                rewards and referral program, resulting in a significant
+                increase in the total number of users of the web application
+              </li>
+              <li>
+                Collaborated with cross-functional agile teams to prioritize and
+                scope feature requests, ensuring that the most impactful
+                features were developed first and delivered on time
+              </li>
+              <li>
+                Communicated regularly with internal stakeholders to identify
+                user pain points and developed solutions to optimize the user
+                experience, resulting in increased customer satisfaction and
+                retention rates
+              </li>
+            </ul>
+          </Tabs.Content>
+          <Tabs.Content value="tab2" className="">
+            <p className="text-slate-300">
+              Application Consultant
+              <span className="text-orange-400"> @ NCS</span>
+            </p>
+            <p className="text-sm">November 2018 - October 2021</p>
+            <br />
+            <ul className="ml-4 list-disc list-outside marker:text-orange-400 gap-1 space-y-4 ">
+              <li>
+                Kickstarted the development of a high-traffic web application as
+                part of Immigration & Checkpoints Authority's Digital
+                Transformation initiative, delivering a reliable and
+                user-friendly platform for millions of users
+              </li>
+              <li>
+                Revamped an outdated web application, improving site performance
+                by 20% and enhancing the user experience through responsive
+                design and streamlined functionality
+              </li>
+              <li>
+                Chaired discussions with stakeholders to identify key
+                requirements and develop tailored solutions to address their
+                specific needs, resulting in increased customer satisfaction and
+                retention
+              </li>
+              <li>
+                Successfully coded, tested, reviewed, and delivered software
+                solutions with a 100% on-time record, meeting or exceeding
+                service level objectives and ensuring high-quality results
+              </li>
+              <li>
+                Optimized web application compatibility and security across
+                multiple platforms, including web and mobile devices, leveraging
+                the latest tools and technologies to enhance performance and
+                mitigate risks.
+              </li>
+            </ul>
+          </Tabs.Content>
+        </Tabs.Root>
+      </div>
+      <div id="work" className="max-w-2xl py-24 w-full">
+        <p className="font-sfmono text-orange-400">
+          03.{" "}
+          <span className="text-3xl font-semibold text-slate-300">
+            Some Things I've built
+          </span>
+        </p>
+        <div className="my-10">
           <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            href="https://motivate-ai.vercel.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className=""
+          >
+            <h4 className="text-slate-300 ">Motivate AI</h4>
+            <p>
+              A motivational quote generator powered by ChatGPT. Just for
+              laughs!
+            </p>
+            <Image
+              src={motivateAiScreen}
+              alt="motivate-ai-screencap"
+              className=" border-2 border-gray-400 hover:border-orange-300 my-2"
+            />
+          </a>
+          <p className="py-4">
+            Technologies used: React, Next.js, Tailwind CSS, OpenAI API
+          </p>
+        </div>
+
+        <div className="my-10">
+          <h4 className="text-slate-300">Portfolio</h4>
+          <p className="">This website. </p>
+          <Image
+            src={darrenPortfolioScreen}
+            alt={"darren-portfolio-screencap"}
+            className="border-2 border-gray-400 my-2"
+          />
+          <p className="py-4">
+            Technologies used: React, Next.js, Tailwind CSS, Radix UI
+          </p>
+        </div>
+      </div>
+      <div id="contact" className="max-w-2xl py-24">
+        <p className="font-sfmono text-orange-400">
+          04.{" "}
+          <span className="text-3xl font-semibold text-slate-300">
+            Get In Touch
+          </span>
+        </p>
+        <p className="my-4 text-justify">
+          I’m currently looking for new opportunities and my inbox is always
+          open. Whether you have a question or just want to say hi, I’ll try my
+          best to get back to you!
+        </p>
+
+        <div className="space-y-2">
+          <a
+            className="hover:text-orange-400 cursor-pointer flex gap-2 w-fit"
+            href="https://linkedin.com/in/darren-cjd"
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
+            <RiLinkedinBoxFill />
+            LinkedIn
+          </a>
+          <a
+            className="hover:text-orange-400 cursor-pointer flex gap-2 w-fit"
+            href="https://github.com/dchewjd"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <RiGithubFill />
+            Github
+          </a>
+          <a
+            className="hover:text-orange-400 cursor-pointer flex gap-2 w-fit"
+            href="mailto:darrencjd@gmail.com"
+          >
+            <RiMailFill />
+            Email
           </a>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
+      <div className="">
+        <p>© 2023 - Darren Chew</p>
       </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
